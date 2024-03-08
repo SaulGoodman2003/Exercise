cc42b47f477b1bbcadfe63221ff8860fc713d7c8
Yu_Gan <jackycheung4396@gmail.com>
2024-03-07 21:22:54 -0500

    1

---
+++ DataStructure/src/main/java/collection/ArrayListExerciseTest.java
@@ -0,0 +1,63 @@
+package collection;
+
+import org.junit.Test;
+
+
+public class ArrayListExerciseTest {
+    /**
+     * new ArrayList()
+     * add elements
+     * get element
+     * get Size
+     * list.addAll(anotherList)
+     */
+    @Test
+    public void learn_Inserting_And_Retrieving() {
+
+    }
+
+    /**
+     * remove(int index)
+     * remove(Object o)
+     * removeRange(int fromIndex, int toIndex)
+     * removeAll(Collection<?> c)
+     * clear()
+     *
+     * Update:
+     * set(int index, E e)
+     * replaceAll(UnaryOperator<E> operator)
+     *
+     * check:
+     * contains(Object o)
+     * indexOf(Object o)
+     * lastIndexOf(Object o)
+     */
+    @Test
+    public void learn_Remove_Replacing_Updating() {
+
+    }
+
+    /**
+     * iterator()
+     * hasNext()
+     * next()
+     * remove()
+     * forEachRemaining(Consumer<? super E> action) -- from Java8
+     */
+
+    @Test
+    public void learn_Iterator() {
+
+    }
+
+    /**
+     * sort(List<T> list)
+     * Collections.sort(List<T> t)
+     * Comparator.reverseOrder()
+     */
+
+    @Test
+    public void learn_Sorting() {
+
+    }
+}
---
+++ DataStructure/src/main/java/collection/CollectionsExerciseTest.java
@@ -0,0 +1,31 @@
+package collection;
+
+import org.junit.Test;
+
+
+public class CollectionsExerciseTest {
+
+    /**
+     * Collections.min(list))
+     * min(Collection c, Comparator comp)
+     *
+     * Collections.max(list)
+     * max(Collection c, Comparator comp)
+     *
+     * frequency(Collection c, object o)
+     */
+
+    @Test
+    public void learn_common_collections_operations() {
+
+    }
+
+    /**
+     * synchronizedList()
+     */
+
+    @Test
+    public void learn_thread_safe_ArrayList() {
+
+    }
+}
